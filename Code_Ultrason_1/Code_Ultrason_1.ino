9b0f5ea4aafe0618d0b3eb24fd674fd4483e9fb9
Forc3ment <kperigault@gmail.com>
2017-10-24 10:30:44 +0200

    skeleton +  pin3

--- Code_Ultrason_1/Code_Ultrason_1.ino
+++ Code_Ultrason_1/Code_Ultrason_1.ino
@@ -1,11 +1,22 @@
-#include <MozziGuts.h>
+
+#include <MozziGuts.h>        // at the top of your sketch
+#define CONTROL_RATE 64   // or some other power of 2
 
 void setup() {
-  // put your setup code here, to run once:
+  startMozzi(CONTROL_RATE);
+}
 
+void updateControl() {
+  // your control code
+}
+
+int updateAudio() {
+  // your audio code which returns an int between -244 and 243
+  // actually, a char is fine
+  return 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  audioHook(); // fills the audio buffer
 }
+
