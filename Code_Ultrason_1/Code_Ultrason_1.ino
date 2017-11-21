6280976c437cbb6a5514e3c3b9afd72b7b908ccb
labolas <laboureur.nicolas@gmail.com>
2017-11-21 12:09:47 +0100

    recuperation 5 last

--- Code_Ultrason_1/Code_Ultrason_1.ino
+++ Code_Ultrason_1/Code_Ultrason_1.ino
@@ -1,12 +1,31 @@
 
 #include <MozziGuts.h>
+#include <Oscil.h>
+#include <tables/sin2048_int8.h>
 #include <math.h>
 
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> sineOsc(SIN2048_DATA);
 // at the top of your sketch
 #define CONTROL_RATE 64   // or some other power of 2
 
 int sound = 0;
 
+//Capteur ultrason
+const int echoPin = 31; // the SRF05's echo pin
+const int initPin = 33; // the SRF05's init pin
+int pulseTime = 0; // variable for reading the pulse
+
+const int pinLedBoot = 13;
+
+
+int freq = 262;
+
+int idx = 0;
+int seuil[23]     = {269, 285, 302, 320, 339, 359, 381, 404, 428, 453, 480, 509, 539, 571, 605, 641, 679, 719, 762, 807, 855, 906, 960};
+int seuillage[24] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988};
+
+int last5[5] = [262,262,262,262,262]; 
+int med[5]   = [262,262,262,262,262];
   
 static const byte sine_tab[256] =
 {
@@ -46,34 +65,65 @@ static const byte sine_tab[256] =
 
 void setup() {
   startMozzi(CONTROL_RATE);
-  Serial.begin(9600);
+  sineOsc.setFreq(freq);
+
+  
+  pinMode(pinLedBoot, OUTPUT);
+  // make the echo pin an input:
+  pinMode(initPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite(pinLedBoot, 1);
+
 }
 
 void updateControl() {
   // your control code
+  digitalWrite(initPin, LOW);
+  delayMicroseconds(2500);
+  digitalWrite(initPin, HIGH); // send signal
+  delayMicroseconds(20); // wait 50 milliseconds for it to return
+  digitalWrite(initPin, LOW); // close signal
+  
+  //100micro à 25milli
+  pulseTime = pulseIn(echoPin, HIGH); // calculate time for signal to return
+
+  freq = map(pulseTime, 100, 25000, 261, 988);
 }
 
 int updateAudio() {
   // your audio code which returns an int between -244 and 243
   // actually, a char is fine
 
-  byte volume = 255;
-  int freq = 440;
-  
-  float rate = 1;
-  
-  float step = 2*M_PI*freq/rate;
+  /*freq=(freq+1)%1000;
+  if(freq>seuil[idx])
+  {
+    idx=(idx+1)%23;
+    sineOsc.setFreq(seuillage[idx]);
+  }
+  */
 
-  sound += step;
-  
-  if(sound >= 2*M_PI)
-    sound-=2*M_PI;
+  for (int i = 0; i < 23; i++){
+    if(freq < seuil[i]){
+      freq = seuillage[i];
+      break;
+    }    
+  }
+  if(freq > seuil[22]){
+      freq = seuillage[23];
+  }
 
-  int res = ((int)(sine_tab[sound]-128) * volume ) >> 8 ;
+  int tmp = last5[0];
+  last5[0]=freq;
+  last5[4]=last5[3];
+  last5[3]=last5[2];
+  last5[2]=last5[1];
+  last5[1]=tmp;
 
-  Serial.println(res);
+  // trier et renvoyer medianne.
+
+  sineOsc.setFreq(freq);
   
-  return res;
+  return sineOsc.next();
 }
 
 void loop() {
